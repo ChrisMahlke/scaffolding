d8c1fd6fa5ef09f535ad12ab189a1b5b5e66080a
Chris Mahlke <cmahlke@gmail.com>
2025-03-18 18:28:51 -0700

    Test Husky setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: { "prettier/prettier": "error" },
+  },
 ];
